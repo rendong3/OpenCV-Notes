80687244f78c14f4de64e7e4406eac459bedaa41
RenDong3 <39895987+RenDong3@users.noreply.github.com>
2019-01-25 21:11:46 +0800

    Add files via upload

---
+++ 6_object_traack/code/6.4_camshift.py
@@ -0,0 +1,81 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import cv2
+
+'''
+    @author 2019-1-25 21:07
+    camshift 
+
+    标记感兴趣区域-->HSV空间 -->计算直方图+归一化 --> 反向投影 --> camshift -->绘框并显示
+
+'''
+
+# 标记初始感兴趣的区域
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+r, h, c, w = 10, 200, 10, 20
+track_window = (c, r, w, h)
+
+roi = frame[r:r + h, c:c + w]
+# BGR ---> HSV
+hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+# 构建mask,给定array上下界
+mask = cv2.inRange(hsv_roi, np.array((100., 30, 32.)), np.array((180, 120, 255)))
+
+# 计算直方图
+roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+
+# 线性直方图归一化
+cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+# 设定meanshift迭代停止条件
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+
+        # 对ROI进行meanshift 给定window和停止条件
+        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+        #返回旋转矩形的四个顶点points
+        pts = cv2.boxPoints(ret)
+        pts = np.int0(pts)
+        #绘制多边形曲线图
+        img2 = cv2.polylines(frame, [pts], True, 255, 2)
+
+        cv2.imshow('img2', img2)
+
+        k = cv2.waitKey(60) & 0xff
+        if k == 27:
+            break
+    else:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
